44f0b9eb51ab5c088c76d4ff556be43248719097
Enrique Martín <enrique.martin.gutierrez@alumnos.upm.es>
2022-06-05 12:48:17 +0200

    aniadida implementacion

---
+++ NotificacionImplementacion/setup.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "swagger_server"
+VERSION = "1.0.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+    "connexion",
+    "swagger-ui-bundle>=0.0.2"
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="API Taller: Notificaciones",
+    author_email="enrique.martin.gutierrez@alumnos.upm.es",
+    url="",
+    keywords=["Swagger", "API Taller: Notificaciones"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    package_data={'': ['swagger/swagger.yaml']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['swagger_server=swagger_server.__main__:main']},
+    long_description="""\
+    Subsistema del envío de notificaciones de eventos relacionados con el taller
+    """
+)
